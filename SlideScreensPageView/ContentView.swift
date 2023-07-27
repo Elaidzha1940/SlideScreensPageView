bab44101fe24835dc0ff3932539f6536111b6ecd
Elaidzha1940 <eli080293@gmail.com>
2023-07-27 19:52:03 +0300

    SlideScreensPageView

---
+++ SlideScreensPageView/ContentView.swift
@@ -0,0 +1,30 @@
+//  /*
+//
+//  Project: SlideScreensPageView
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.07.2023
+//
+//  S
+//
+//  /*
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlideScreensPageView/SlideScreensPageViewApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: SlideScreensPageView
+//  File: SlideScreensPageViewApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.07.2023
+//
+//  S
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SlideScreensPageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
