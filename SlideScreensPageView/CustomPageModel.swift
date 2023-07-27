b625a40d19918c3d202faac1df0a7d7f8c514283
Elaidzha1940 <eli080293@gmail.com>
2023-07-27 19:56:37 +0300

    SlideScreensPageView + added CustomPageModel

---
+++ SlideScreensPageView/CustomPageModel.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: SlideScreensPageView
+//  File: CustomPageModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: on 27.07.2023.
+//
+//  S
+//
+//  */
+
+import Foundation
+
+struct CustomPage: Identifiable {
+    
+    let id = UUID()
+    var name: String
+    var description: String
+    var ImageUrl: String
+}
+
+
